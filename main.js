279fbf3a52e88784459b7e0e6a7b4354025b829f
Julian Erre <julian.erreguerena@gmail.com>
2021-10-13 23:27:46 -0300

    hice la funcion de carrito

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const API = '../api.json';
+let  carrito = {};
 
 const getData = (apiUrl) => {
     return fetch(apiUrl)
@@ -19,13 +20,36 @@ const printData = (data) => {
                 '<img src="' + c.img + '" class="imgCart">' +
                 '<span class="precio">$' + c.precio + '</span>' +
                 '<p class="descripcion">' + c.title + '</p>' +
-                '<button class="btn-grad"> ' + "comprar" + '</button>' +
+                '<button class="btn-grad" id="'+ c.id +'"> ' + "comprar" + '</button>' +
             '</div>'+
             '</div>'  
 
     });
     
     $(".productos").html(html);
+
+}
+
+$(".productos").click(e => {
+    
+    //console.log(e.target)
+    //console.log(e.target.classList.contains('btn-grad'))
+    if(e.target.classList.contains('btn-grad')){
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation();
+})
+
+const setCarrito = objeto => {
+    console.log(objeto)
+    const producto = {
+        id: objeto.querySelector('.btn-grad').id,
+        title: objeto.querySelector('p').textContent,
+        precio: objeto.querySelector('span').textContent
+
+    }
+    console.log(producto);
+
 
 }
 
